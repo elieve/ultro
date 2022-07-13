931d15a1c2fd6746358047660d4cb49d19183f25
elieve <bayustiadi@ymail.com>
2022-07-13 21:54:12 +0800

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -30,14 +30,14 @@ from . import _ult_cache
 
 SUP_BUTTONS = [
     [
-        Button.url("• Repo •", url="https://github.com/TeamUltroid/Ultroid"),
-        Button.url("• Support •", url="t.me/UltroidSupport"),
+        Button.url("• Repo •", url="https://github.com/elieve/ultro"),
+        Button.url("• Support •", url="t.me/ygabutkan"),
     ],
 ]
 
-ofox = "https://telegra.ph/file/231f0049fcd722824f13b.jpg"
-gugirl = "https://telegra.ph/file/0df54ae4541abca96aa11.jpg"
-ultpic = "https://telegra.ph/file/4136aa1650bc9d4109cc5.jpg"
+ofox = "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg"
+gugirl = "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg"
+ultpic = "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg"
 
 apis = [
     "QUl6YVN5QXlEQnNZM1dSdEI1WVBDNmFCX3c4SkF5NlpkWE5jNkZV",
@@ -167,14 +167,14 @@ async def _(e):
 async def repo(e):
     res = [
         await e.builder.article(
-            title="Ultroid Userbot",
+            title="BayUserbot",
             description="Userbot | Telethon",
             thumb=wb(ultpic, 0, "image/jpeg", []),
-            text="• **ULTROID USERBOT** •",
+            text="• **BAY USERBOT** •",
             buttons=SUP_BUTTONS,
         ),
     ]
-    await e.answer(res, switch_pm="Ultroid Repo.", switch_pm_param="start")
+    await e.answer(res, switch_pm="BayUserBot Repo.", switch_pm_param="start")
 
 
 @in_pattern("go", owner=True)
@@ -395,7 +395,7 @@ async def piston_run(event):
             title="Bad Query",
             description="Usage: [Language] [code]",
             thumb=wb(
-                "https://telegra.ph/file/e33c57fc5f1044547e4d8.jpg", 0, "image/jpeg", []
+                "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg", 0, "image/jpeg", []
             ),
             text=f'**Inline Usage**\n\n`@{asst.me.username} run python print("hello world")`\n\n[Language List](https://telegra.ph/Ultroid-09-01-6)',
         )
@@ -410,7 +410,7 @@ async def piston_run(event):
             title="Unsupported Language",
             description="Usage: [Language] [code]",
             thumb=wb(
-                "https://telegra.ph/file/e33c57fc5f1044547e4d8.jpg", 0, "image/jpeg", []
+                "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg", 0, "image/jpeg", []
             ),
             text=f'**Inline Usage**\n\n`@{asst.me.username} run python print("hello world")`\n\n[Language List](https://telegra.ph/Ultroid-09-01-6)',
         )
@@ -429,7 +429,7 @@ async def piston_run(event):
         description=output,
         text=f"• **Language:**\n`{lang}`\n\n• **Code:**\n`{code}`\n\n• **Result:**\n`{output}`",
         thumb=wb(
-            "https://telegra.ph/file/871ee4a481f58117dccc4.jpg", 0, "image/jpeg", []
+            "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg", 0, "image/jpeg", []
         ),
         buttons=Button.switch_inline("Fork", query=event.text, same_peer=True),
     )
@@ -533,7 +533,7 @@ async def koo_search(ult):
             profileImage = (
                 item["profileImageBaseUrl"]
                 if item.get("profileImageBaseUrl")
-                else "https://telegra.ph/file/dc28e69bd7ea2c0f25329.jpg"
+                else "https://telegra.ph/file/ca5c07f5566431e1e6152.jpg"
             )
             extra = await async_searcher(
                 "https://www.kooapp.com/apiV1/users/handle/" + item["userHandle"],
